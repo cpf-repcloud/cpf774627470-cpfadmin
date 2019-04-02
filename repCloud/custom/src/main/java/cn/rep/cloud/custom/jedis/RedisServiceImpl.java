14c8924ab52f4d5cb94595e456282a4f086b7b1c
e <443425833@qq.com>
2019-04-02 19:44:49 +0800

    redis配置

---
+++ repCloud/custom/src/main/java/cn/rep/cloud/custom/jedis/RedisServiceImpl.java
@@ -0,0 +1,12 @@
+package cn.rep.cloud.custom.jedis;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisServiceImpl extends IRedisService<RedisModel> {
+    private static final String REDIS_KEY = "TEST_REDIS_KEY";
+    @Override
+    protected String getRedisKey() {
+        return this.REDIS_KEY;
+    }
+}
