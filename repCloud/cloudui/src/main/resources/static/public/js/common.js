8027c4131c00611374f4dc1a4d0a7e4f579ead2e
e <443425833@qq.com>
2019-04-12 11:04:47 +0800

    解决input框右边一直转的问题

--- repCloud/cloudui/src/main/resources/static/public/js/common.js
+++ repCloud/cloudui/src/main/resources/static/public/js/common.js
@@ -2,4 +2,3 @@ document.write('<script type="text/javascript" src="../../static/public/js/vue.m
 document.write('<script type="text/javascript" src="../../static/public/js/iview.min.js"></script>')
 document.write('<script type="text/javascript" src="../../static/public/js/jquery.min.js"></script>')
 document.write('<script type="text/javascript" src="../../static/public/js/vue.extend.js"></script>')
-document.write('<script type="text/javascript" src="../../static/public/js/ivetech.min.js"></script>')
