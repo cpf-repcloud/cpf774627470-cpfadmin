561eabf0f192930dd5aefeb3ca087935262660f9
e <443425833@qq.com>
2019-05-13 20:02:25 +0800

    城市下拉控件

--- repCloud/custom/src/main/java/cn/rep/cloud/custom/coreutils/common/StartService.java
+++ repCloud/custom/src/main/java/cn/rep/cloud/custom/coreutils/common/StartService.java
@@ -1,7 +1,9 @@
 package cn.rep.cloud.custom.coreutils.common;
 
 import cn.rep.cloud.custom.basecommon.common.Constants;
+import cn.rep.cloud.custom.basicdata.business.RepCityServiceImpl;
 import cn.rep.cloud.custom.basicdata.business.RepCountryServiceImpl;
+import cn.rep.cloud.custom.basicdata.entity.RepCity;
 import cn.rep.cloud.custom.basicdata.vo.RepCountryVO;
 import cn.rep.cloud.custom.coreutils.jedis.RedisModel;
 import cn.rep.cloud.custom.coreutils.jedis.RedisServiceImpl;
@@ -26,11 +28,24 @@ public class StartService implements ApplicationRunner {
     private RedisServiceImpl redisService;
     @Autowired
     private RepCountryServiceImpl repCountryService;
+    @Autowired
+    private RepCityServiceImpl repCityService;
 
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         this.saveCountryCityKj();
+        this.queryXlCityKj();
+    }
+
+    private void queryXlCityKj(){
+        //首先将城市下拉控件缓存删除
+        redisService.removeByKey(Constants.CITY_XLKJ);
+        //再将城市下拉控件缓存
+        RedisModel redisModel = new RedisModel<>();
+        List<RepCity> list = repCityService.getXlCityKj();
+        redisModel.setResultLists(list);
+        redisService.put(Constants.CITY_XLKJ, SerializeUtil.serialize(redisModel),-1);
     }
 
     private void saveCountryCityKj(){
